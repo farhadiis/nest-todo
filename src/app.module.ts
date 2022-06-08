cbba4d90bf148a5c51e05893286c2165ffa4702e
farhadiis <me.ehyaie@gmail.com>
2022-06-08 14:00:31 +0430

    prepare todo controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app/app.controller';
+import { AppService } from './app/app.service';
+import { TodoController } from './todo/todo.controller';
+import { TodoService } from './todo/todo.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TodoController],
+  providers: [AppService, TodoService],
 })
 export class AppModule {}
---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+
+@Controller('todo')
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Post()
+  async create(@Body() createTodoDto: CreateTodoDto) {
+    console.log(createTodoDto);
+    return 'This action adds a new cat';
+  }
+}
