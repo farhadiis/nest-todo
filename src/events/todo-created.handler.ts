b0a1e81d8eb6e6d3cad8f6b54b3d9d772169f5c0
farhadiis <me.ehyaie@gmail.com>
2022-06-12 09:42:19 +0430

    remove unused

--- src/events/todo-created.handler.ts
+++ src/events/todo-created.handler.ts
@@ -1,15 +1,12 @@
 import { IEventHandler } from '@nestjs/cqrs';
 import { EventsHandler } from '@nestjs/cqrs/dist/decorators/events-handler.decorator';
 import { TodoCreatedEvent } from './todo-created.event';
-import { EventStoreDbService } from '../services/event-store-db.service';
 import { TodoService } from '../services/todo.service';
 
 @EventsHandler(TodoCreatedEvent)
 export class TodoCreatedHandler implements IEventHandler<TodoCreatedEvent> {
-  constructor(
-    private readonly eventStoreDbService: EventStoreDbService,
-    private readonly todoService: TodoService,
-  ) {}
+  constructor(private readonly todoService: TodoService) {}
+
   async handle(event: TodoCreatedEvent) {
     console.log('TodoCreatedEvent...');
     await this.todoService.store(event);
